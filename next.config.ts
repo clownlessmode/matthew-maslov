720fb3531f2e5734ecb5afae098dc0018f0b7b8d
clownlessmode <clownessmode@bk.ru>
2025-07-10 20:56:50 +0700

    fix

--- next.config.ts
+++ next.config.ts
@@ -5,9 +5,9 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  // typescript: {
+  //   ignoreBuildErrors: true,
+  // },
   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
 };
 
